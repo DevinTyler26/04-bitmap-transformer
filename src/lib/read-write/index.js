d5b9af6cf9b45ed417afb44c6f6c8034a9820401
Kevin Hwangpo <hwangpo3@gmail.com>
2018-06-15 14:02:57 -0700

    added write function, tested txt

--- src/lib/read-write/index.js
+++ src/lib/read-write/index.js
@@ -8,17 +8,25 @@ const RW = module.exports = {};
 RW.read = function (filePath, callback) { //eslint-disable-line
   fs.readFile(filePath, (err, data) => {
     if (err) callback(err); 
-    callback(null, data)
+    callback(null, data);
   }); 
-
 };
-// make a function to hook up write to empty object
-RW.write = function (buffer, filePath, callback) {
 
+// make a function to hook up write to empty object
+RW.write = function (filePath, buffer, callback) { //eslint-disable-line
+  fs.writeFile(filePath, buffer, (err) => {
+    if (err) throw err;
+    callback(null, buffer);
+  });
 };
 
 RW.read('../../assets/bitmap.bmp', (err, data) => {
-  if(err) throw err
-  console.log('return from READ',data);
-  return data
+  if (err) throw err;
+  console.log('return from READ', data);
+  return data;
+});
+
+RW.write('../../assets/new.txt', 'hello', (err) => {
+  if (err) throw err;
+  console.log('return from WRITE');
 });
